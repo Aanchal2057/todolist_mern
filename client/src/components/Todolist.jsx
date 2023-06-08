1368db1d29b44a87f73a446a266189d43542b2b4
“aanchal2057” <aanchalneupane@gmail.com>
2023-06-08 21:38:41 +0545

    message

---
+++ client/src/components/Todolist.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todolist = () => {
+  return (
+    <form className='form'>
+        <input 
+            placeholder='Enter new todo...'
+            className='input'
+        />
+    </form>
+  )
+}
+
+export default Todolist
