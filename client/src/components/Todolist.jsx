dbae75db75f3db86cf06859f65b7ab24fff2b7e5
“aanchal2057” <aanchalneupane@gmail.com>
2023-06-09 06:39:12 +0545

    backend setup going

--- client/src/components/Todolist.jsx
+++ client/src/components/Todolist.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Todolist = () => {
+ const[text,setText] = useState("");
+  const onFormSubmit = () =>{
+
+  }
+  const onInputChange = (e) =>{
+    setText(e.target.value);
+  }
   return (
-    <form className='form'>
+  
+    <form className='form' onSubmit={onFormSubmit}>
         <input 
             placeholder='Enter new todo...'
             className='input'
+            onChange={onInputChange}
         />
     </form>
   )
