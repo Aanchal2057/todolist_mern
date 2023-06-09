9bb2f2b2059cbde15820c11878d938b3830dc000
“aanchal2057” <aanchalneupane@gmail.com>
2023-06-09 08:39:45 +0545

    databse connected

---
+++ server/database/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+ const Connection = () =>{
+    const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@mern-todolist.xfbbgg7.mongodb.net/`
+   
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+    mongoose.connection.on('connected',()=>{
+        console.log('Database connected successfully');
+    })
+    mongoose.connection.on('disconnected',()=>{
+      console.log('Database disconnected');
+    })
+    mongoose.connection.on('error',()=>{
+        console.log('Error while connecting with the database',error.mess);
+      })
+}
+export default Connection;
